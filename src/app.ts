9f273f1a0a2175ca7d3d788fe8efc1c7208dcb05
gouravkotnala <gouravkotnala777@gmail.com>
2024-06-20 12:43:33 +0530

    feat : connect Mongodb Database using mongoose & configure bodyParser

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { errorMiddleware } from "./middlewares/errorMiddleware";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import cors from "cors";
+import connectDatabase from "./config/db";
 
 config({path:"./.env"});
 
@@ -12,11 +13,14 @@ const app = express();
 
 
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cors({
     origin:[`${process.env.CLIENT_URL}` as string],
     credentials: true
 }));
+
+connectDatabase(process.env.DATABASE_URI as string);
 
 app.use("/api/v1/user", userRouter);
 
