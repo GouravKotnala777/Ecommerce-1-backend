4941be708f64b4f26d66e8bccd7a516d79085429
gouravkotnala <gouravkotnala777@gmail.com>
2024-06-20 13:14:15 +0530

    feat : add custom error for CastError in errorMiddleware.ts

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -5,11 +5,18 @@ import { ErrorHandler } from "../utils/utilities";
 
 export const errorMiddleware = (err:ErrorHandler, req:Request, res:Response, next:NextFunction) => {
     const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    // console.log("))))))))))))))))))))))))))");
+    let message:string;
+
+    if (err.name === "CastError") {
+        message = "Wrong ObjectId formate";
+    }
+    else{
+        message = err.message || "Internal Server Error";
+    }
+    console.log("))))))))))))))))))))))))))");
     // console.log(err);
-    // console.log(err.message);
-    // console.log("((((((((((((((((((((((((((");
+    console.log(message);
+    console.log("((((((((((((((((((((((((((");
     
 
     res.status(statusCode).json({success:false, message});
