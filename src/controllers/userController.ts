bf6d40dec0b066e5f487c2a5491b2c413419dfac
gouravkotnala <gouravkotnala777@gmail.com>
2024-06-22 19:13:46 +0530

    refactor : cut token generating and storing in cookie part from login controller and make a utility function of it & use that utility function in both (login and register) controller & feat : now user don't need further login after a successfull registration

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/userModel";
-import {ErrorHandler} from "../utils/utilities";
+import {ErrorHandler, sendToken} from "../utils/utilities";
 import { AuthenticatedUserRequest } from "../middlewares/auth";
 import mongoose from "mongoose";
 
@@ -10,17 +10,19 @@ export const register = async(req:Request, res:Response, next:NextFunction) => {
 
         console.log({name, email, password, avatar, mobile});
 
-        const user = await User.findOne({email});
+        const isUserExist = await User.findOne({email});
         
-        console.log({user});
+        console.log({isUserExist});
 
-        if (user) return next(new ErrorHandler("User already exists", 401));
+        if (isUserExist) return next(new ErrorHandler("User already exists", 401));
 
         const newUser = await User.create({
             name, email, password, avatar, mobile
         });
 
         if (!newUser) return next(new ErrorHandler("Internal Server Error", 500));
+
+        await sendToken(newUser, res, next);
         
         res.status(200).json({success:true, message:"Registration successfull"});
     } catch (error) {
@@ -43,12 +45,7 @@ export const login  = async(req:Request, res:Response, next:NextFunction) => {
 
         if (!isPasswordMatched) return (next(new ErrorHandler("Wrong email or password 2", 404)));
 
-        const token = await isUserExist.generateToken(isUserExist._id);
-
-        console.log({token});
-        
-
-        res.cookie("userToken", token, {expires: new Date(Date.now() + 604800000)})
+        await sendToken(isUserExist, res, next);
 
         res.status(200).json({success:true, message:"Login successfull"});
     } catch (error) {
