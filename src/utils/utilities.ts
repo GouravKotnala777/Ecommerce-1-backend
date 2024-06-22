994d6fa269a878d1528ee6793d13ea0b13605e6b
gouravkotnala <gouravkotnala777@gmail.com>
2024-06-22 19:08:08 +0530

    feat : make a utility function named sendToken to generate token and store it in cookie

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -1,5 +1,6 @@
-import { NextFunction } from "express";
-import { Model } from "mongoose";
+import { NextFunction, Response } from "express";
+import mongoose, { Model } from "mongoose";
+import { UserTypes } from "../models/userModel";
 
 
 
@@ -11,6 +12,18 @@ export class ErrorHandler extends Error {
         // Error.captureStackTrace(this, this.constructor);
     }
 };
+export const sendToken = async(model:(mongoose.Document<unknown, {}, UserTypes> & UserTypes & Required<{
+    _id: mongoose.Schema.Types.ObjectId;
+}>) | null, res:Response, next:NextFunction) => {
+    try {
+        const token = await model?.generateToken(model?._id);
+
+        res.cookie("userToken", token, {expires: new Date(Date.now() + 604800000)})
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
 
 export const throwError = (user:any, errorMessage:string, errorCode:number, next:NextFunction) => {
     console.log("))))))))))))))))))))))");
