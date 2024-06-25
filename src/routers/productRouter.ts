52ce1c57b707decd7b588f0a31137d79b052425f
gouravkotnala <gouravkotnala777@gmail.com>
2024-06-25 18:33:29 +0530

    feat : use multer.middleware in productRouter.ts

--- src/routers/productRouter.ts
+++ src/routers/productRouter.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import { isUserAdmin, isUserAuthenticated } from "../middlewares/auth";
 import { allProducts, createProduct, deleteProduct, singleProducts, updateProduct } from "../controllers/productController";
+import upload from "../middlewares/multer.middleware";
 
 const productRouter = express.Router();
 
 
 
 productRouter.route("/all").get(allProducts);
-productRouter.route("/new").post(isUserAuthenticated, isUserAdmin, createProduct);
+productRouter.route("/new").post(isUserAuthenticated, isUserAdmin, upload.single("photo"), createProduct);
 productRouter.route("/productID").get(singleProducts)
                                 .delete(isUserAuthenticated, isUserAdmin, deleteProduct)
                                 .put(isUserAuthenticated, isUserAdmin, updateProduct);
