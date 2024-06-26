d411269722053e0b64db753d34689ae6c6f3b446
gouravkotnala <gouravkotnala777@gmail.com>
2024-06-26 12:03:32 +0530

    refactor : config cloudinary in app.ts and remove from cloudinary.util.ts

--- src/utils/cloudinary.util.ts
+++ src/utils/cloudinary.util.ts
@@ -1,24 +1,14 @@
 import {v2 as cloudinary} from "cloudinary";
 import fs from "fs";
 
-
-cloudinary.config({
-  cloud_name:process.env.CLOUDINARY_CLOUD_NAME as string,
-  api_key:process.env.CLOUDINARY_API_KEY as string,
-  api_secret:process.env.CLOUDINARY_API_SECRET as string
-});
-
+  
 export const uploadOnCloudinary = async(localFilePath:string) => {
   try {
 
     if (!localFilePath) {
-      console.log({localFilePath});
       return null
     }
     const res = await cloudinary.uploader.upload(localFilePath);
-
-    console.log({res});
-    
 
     if (res.url) {
       console.log("File is uploaded successfully");
