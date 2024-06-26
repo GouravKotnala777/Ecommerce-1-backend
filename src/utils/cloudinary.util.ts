7e3729eb3b744867b470a075295cf16b599def7c
gouravkotnala <gouravkotnala777@gmail.com>
2024-06-26 12:10:51 +0530

    feat : specify uploaded photo destination folder in cloudinary in cloudinary.util.ts

--- src/utils/cloudinary.util.ts
+++ src/utils/cloudinary.util.ts
@@ -2,13 +2,15 @@ import {v2 as cloudinary} from "cloudinary";
 import fs from "fs";
 
   
-export const uploadOnCloudinary = async(localFilePath:string) => {
+export const uploadOnCloudinary = async(localFilePath:string, cloudinaryDestinationFoler:string) => {
   try {
 
     if (!localFilePath) {
       return null
     }
-    const res = await cloudinary.uploader.upload(localFilePath);
+    const res = await cloudinary.uploader.upload(localFilePath, {
+      folder:cloudinaryDestinationFoler
+    });
 
     if (res.url) {
       console.log("File is uploaded successfully");
