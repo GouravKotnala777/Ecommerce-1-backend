bce249ad5d49b69311794b10012c45d58e505e13
gouravkotnala <gouravkotnala777@gmail.com>
2024-08-24 16:19:32 +0530

    feat(app.ts) : add some socket.io events

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,8 @@ import couponRouter from "./routers/couponRouter";
 import paymentRouter from "./routers/paymentRouter";
 import Stripe from "stripe";
 import orderRouter from "./routers/orderRouter";
+import http from "http";
+import { Server } from "socket.io";
 
 config({path:"./.env"});
 
@@ -22,6 +24,14 @@ const DATABASE_URI = process.env.DATABASE_URI || "";
 const STRIPE_KEY = process.env.STRIPE_KEY || "";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors:{
+        origin:"http://127.0.0.1:5173",
+        methods:["GET", "POST"],
+        credentials:true
+    }
+});
 
 
 app.use(cookieParser());
@@ -57,6 +67,76 @@ app.get("/test", (req, res, next) => {
 
 app.use(errorMiddleware);
 
-app.listen(PORT, () => {
+
+
+//const usersa = {
+//    "456789987654121":{socketID:"gourav123", userName:"Gourav"}
+//    undefined:{socketID:"66bb37913c60c222387502ed", userName:undefined}
+//}
+//console.log(usersa[456789987654121].socketID);
+
+
+let users:{[userID:string]:{socketID:string, userName:string};} = {};
+
+io.on("connection", (socket) => {
+    //console.log(`${socket.id}connected...`);
+
+    socket.on("registerUser", ({userID, userName}) => {        
+        if (userID) {
+            users[userID] = {socketID:socket.id, userName:userName};
+            //console.log(`${userID} -----> ${users[userID]?.socketID} = ${users[userID]?.userName}`);
+            io.to(users["66bb37913c60c222387502ed"]?.socketID).emit("usersList", users);
+        }
+    });
+
+    socket.on("adminSelectedUser", ({userID, adminName, defaultMsg}) => {
+        io.to(users[userID]?.socketID).emit("adminSelectedUserBE", {adminName, defaultMsg});
+    });
+
+    socket.on("adminEndedChat", ({userID, defaultMsg}) => {
+        io.to(users[userID]?.socketID).emit("connectionEnded", {defaultMsg});
+    });
+
+    socket.on("userEndedChat", ({defaultMsg}) => {
+        io.to(users["66bb37913c60c222387502ed"]?.socketID).emit("connectionEnded", {defaultMsg});
+        for (let userid in users) {
+            if (users[userid]?.socketID === socket.id) {
+                delete users[userid];
+                io.to(users["66bb37913c60c222387502ed"]?.socketID).emit("usersList", users);
+                break;
+            }
+        }
+        io.to(users["66bb37913c60c222387502ed"]?.socketID).emit("usersList", users);
+    });
+
+    socket.on("userMessage", ({userID, userName, msg}) => {
+        //console.log(`${userID}:${msg}`);
+        if (users["66bb37913c60c222387502ed"] && users["66bb37913c60c222387502ed"]?.socketID) {
+            io.to(users["66bb37913c60c222387502ed"]?.socketID).emit("adminReceiveMessage", {userID, userName, msg});
+        }
+    });
+
+    socket.on("adminResponse", ({adminID, userID, userName, response}) => {
+        //console.log(`response ${response} to ${userID} (${users[userID]})`);
+        io.to(users[userID]?.socketID).emit("userReceiveResponse", {adminID, userName, response});
+    });
+
+    socket.on("disconnect", () => {
+        //console.log(`${socket.id}} user disconnected`);
+        for (let userid in users) {
+            if (users[userid]?.socketID === socket.id) {
+                delete users[userid];
+                io.to(users["66bb37913c60c222387502ed"]?.socketID).emit("usersList", users);
+                break;
+            }
+        }
+    });
+    
+})
+
+
+
+
+server.listen(PORT, () => {
     console.log("Listening....");
 })
