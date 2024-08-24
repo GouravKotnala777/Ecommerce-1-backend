e92f5d43abbc66490fed684d260017efd4da84bf
gouravkotnala <gouravkotnala777@gmail.com>
2024-08-24 20:33:06 +0530

    refactor(app.ts) : add url for socket.io

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin:"http://127.0.0.1:5173",
+        origin:process.env.CLIENT_URL,
         methods:["GET", "POST"],
         credentials:true
     }
