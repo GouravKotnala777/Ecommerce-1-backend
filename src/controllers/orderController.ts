b7247c9f3f8b6c48286034037db6299eae9d113a
gouravkotnala <gouravkotnala777@gmail.com>
2024-09-20 20:05:30 +0530

    feat(orderController.ts) : add allOrders function for admin

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -106,11 +106,25 @@ export const myOrders = async(req:Request, res:Response, next:NextFunction) => {
         
         if (!userID) return next(new ErrorHandler("userID not found", 404));
         
-        const orders = await Order.find({userID}).populate({model:"Product", path:"orderItems.productID", select:"name price images"});
+        const orders = await Order.find({userID}).populate({model:"Product", path:"orderItems.productID", select:"name price images category"});
         //console.log(orders);
         //if (orders.length === 0) return next(new ErrorHandler(["You have not ordered anything yet!"], 204));
 
         res.status(200).json({success:true, message:orders});
+    } catch (error) {
+        console.log(error);
+        next(error);        
+    }
+};
+export const allOrders = async(req:Request, res:Response, next:NextFunction) => {
+    try {
+        const userID = (req as AuthenticatedUserRequest).user._id;
+        
+        if (!userID) return next(new ErrorHandler("userID not found", 404));
+        
+        const allOrders = await Order.find().populate({model:"Product", path:"orderItems.productID", select:"name price images category"});
+
+        res.status(200).json({success:true, message:allOrders});
     } catch (error) {
         console.log(error);
         next(error);        
--- src/routers/orderRouter.ts
+++ src/routers/orderRouter.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { isUserAuthenticated } from "../middlewares/auth";
-import { myOrders, newOrder } from "../controllers/orderController";
+import { isUserAdmin, isUserAuthenticated } from "../middlewares/auth";
+import { allOrders, myOrders, newOrder } from "../controllers/orderController";
 import { updateActivity } from "../middlewares/userActivity.middleware";
 const orderRouter = express.Router();
 
 orderRouter.route("/new").post(isUserAuthenticated, newOrder, updateActivity);
 orderRouter.route("/myOrders").get(isUserAuthenticated, myOrders);
+orderRouter.route("/all").get(isUserAuthenticated, isUserAdmin, allOrders);
 
 export default orderRouter;
