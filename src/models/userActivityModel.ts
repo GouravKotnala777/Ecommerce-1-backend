3580dce12ca00e68750ddf78caec6330d9abd175
gouravkotnala <gouravkotnala777@gmail.com>
2024-10-02 11:50:15 +0530

    refactor(userActivityModel.ts) : add some values ('update_order' & 'remove_product_from_order') for enum of ction field

--- src/models/userActivityModel.ts
+++ src/models/userActivityModel.ts
@@ -4,7 +4,7 @@ import { UserLocationTypes } from "../controllers/userController";
 
 export interface UserActivityType extends Document {
     userID: mongoose.Types.ObjectId|null;
-    action: "signin"|"logout"|"register"|"verify_email"|"forget_password"|"password_change"|"update_profile"|"add_address"|"remove_address"|"create_review"|"delete_review"|"update_vote"|"update_wishlist"|"search_product"|"create_product"|"add_to_cart"|"remove_from_cart"|"create_payment_intend"|"create_payment_intend_again"|"new_order"|"new_order_fail"|"create_coupon"|"new_chat"|"is_chat_helpful"; // e.g., "signin", "logout", "register", "password_change"
+    action: "signin"|"logout"|"register"|"verify_email"|"forget_password"|"password_change"|"update_profile"|"add_address"|"remove_address"|"create_review"|"delete_review"|"update_vote"|"update_wishlist"|"search_product"|"create_product"|"add_to_cart"|"remove_from_cart"|"create_payment_intend"|"create_payment_intend_again"|"new_order"|"new_order_fail"|"create_coupon"|"new_chat"|"is_chat_helpful"|"update_order"|"remove_product_from_order"; // e.g., "signin", "logout", "register", "password_change"
     ipAddress: string;
     userAgent: string;
     userLocation: UserLocationTypes; // e.g., user's physical location based on IP
@@ -33,7 +33,7 @@ const userActivitySchema = new mongoose.Schema<UserActivityType>({
             "search_product", "create_product",
             "add_to_cart", "remove_from_cart",
             "create_payment_intend", "create_payment_intend_again",
-            "new_order", "new_order_fail", "create_coupon",
+            "new_order", "new_order_fail", "update_order", "remove_product_from_order", "create_coupon",
             "new_chat", "is_chat_helpful"
         ],
         required:true
