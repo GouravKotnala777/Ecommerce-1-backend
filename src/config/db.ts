af09e6acedd0510bb7e58f471fce886913f36447
Gourav Kotnala <125181478+GouravKotnala777@users.noreply.github.com>
2026-03-27 12:22:52 +0530

    refactor : refactor connectDatabase from then/catch to async/await
    Refactor connectDatabase function to use async/await and handle undefined databaseURI.

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,23 @@
 import mongoose from "mongoose";
 
-const connectDatabase = (databaseURI:string) => {
-    mongoose.connect(databaseURI, {
-        dbName:"Ecommerce1"
-    }).then(() => {console.log("database...");})
-    .catch((error) => {console.log(error);})
+// const connectDatabase = (databaseURI:string) => {
+//     mongoose.connect(databaseURI, {
+//         dbName:"Ecommerce1"
+//     }).then(() => {console.log("database...");})
+//     .catch((error) => {console.log(error);})
+// };
+
+async function connectDatabase(databaseURI:string|undefined){
+    try {
+        if (!databaseURI) {
+            throw new Error(`databaseURI is undefined ${databaseURI}`);
+        }
+        const db = await mongoose.connect(databaseURI);
+        console.log("database...");
+        
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export default connectDatabase;
